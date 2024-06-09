7a28377e2840d4913f9bace0764aa66c8c7e9160
FengXiang2233 <fjx13125@163.com>
2024-06-09 19:48:31 +0800

    完成部分机器基类

---
+++ src/main/java/me/fengxiang/XingChengCraft/obj/machine/BasicMachine.kt
@@ -0,0 +1,48 @@
+package me.fengxiang.XingChengCraft.obj.machine
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.interfaces.InventoryBlock
+import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset
+import org.bukkit.block.Block
+import org.bukkit.inventory.ItemStack
+
+
+abstract class BasicMachine(
+        itemGroup: ItemGroup,
+        item: SlimefunItemStack,
+        recipeType: RecipeType,
+        recipe: Array<out ItemStack>
+) : SlimefunItem(itemGroup, item, recipeType, recipe), InventoryBlock{
+
+    init {
+        this.createPreset(this, this.getInventoryTitle(), this::constructMenu)
+    }
+
+    private fun getInventoryTitle(): String {
+        return this.itemName
+    }
+
+    // 通过重写constructMenu实现gui
+    abstract fun constructMenu(blockMenuPreset: BlockMenuPreset)
+
+    override fun preRegister() {
+        addItemHandler(object : BlockTicker() {
+            override fun tick(b: Block, sf: SlimefunItem, data: Config) {
+                this@BasicMachine.tick(b)
+            }
+
+            override fun isSynchronized(): Boolean {
+                return false
+            }
+        })
+    }
+
+    // 通过重写tick来工作
+    abstract fun tick(b:Block)
+
+}
---
+++ src/main/java/me/fengxiang/XingChengCraft/obj/machine/ElectricMachine.kt
@@ -0,0 +1,52 @@
+package me.fengxiang.XingChengCraft.obj.machine
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent
+import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType
+import me.mrCookieSlime.Slimefun.api.BlockStorage
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu
+import org.bukkit.Location
+import org.bukkit.block.Block
+import org.bukkit.inventory.ItemStack
+import javax.annotation.Nonnull
+
+abstract class ElectricMachine(
+    itemGroup: ItemGroup,
+    item: SlimefunItemStack,
+    recipeType: RecipeType,
+    recipe: Array<out ItemStack>
+) : BasicMachine(itemGroup, item, recipeType, recipe), EnergyNetComponent {
+
+    override fun tick(b: Block) {
+        val inv = BlockStorage.getInventory(b)
+        if (takeCharge(b.location)) {
+            if (findNextRecipe(inv)) setCharge(b.location, this.getCharge(b.location) - getEnergyConsumption())
+        }
+    }
+
+    private fun takeCharge(@Nonnull l: Location): Boolean {
+        return if (this.isChargeable) {
+            val charge = this.getCharge(l)
+            charge >= getEnergyConsumption()
+        } else {
+            true
+        }
+    }
+
+    override fun getEnergyComponentType(): EnergyNetComponentType {
+        return EnergyNetComponentType.CONSUMER
+    }
+
+    override fun getCapacity(): Int {
+        return 1
+    }
+
+    private fun getEnergyConsumption(): Int {
+        return 1
+    }
+
+    abstract fun findNextRecipe(inv: BlockMenu):Boolean
+
+}

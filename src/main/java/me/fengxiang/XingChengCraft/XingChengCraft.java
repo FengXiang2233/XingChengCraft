cbfbdd53bd3845bc0e84265b9c486741238ca749
fengxiang2233 <fjx13125@163.com>
2024-07-26 13:22:31 +0800

    init

---
+++ src/main/java/me/fengxiang/XingChengCraft/XingChengCraft.java
@@ -0,0 +1,36 @@
+package me.fengxiang.XingChengCraft;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class XingChengCraft extends JavaPlugin implements SlimefunAddon {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Nonnull
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return this;
+    }
+
+    @Override
+    public String getBugTrackerURL() {
+        return "https://github.com/FengXiang2233/XingChengCraft/issues";
+    }
+}
